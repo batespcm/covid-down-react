96c37ee5bd6183cbfb81e297fd9c69d00209db36
Lee <batespcm1981@gmail.com>
2020-06-17 14:46:53 +0100

    css fixed for letter tables and footertable after seeing it on a larger screen

--- src/components/NumbersRules.js
+++ src/components/NumbersRules.js
@@ -3,11 +3,11 @@ import React from 'react';
 const NumbersRules = () => {
   return (
     <ul className="NumbersRules">
-      <li>Select from Small (1-10) and Big (25,50,75,100) Numbers</li>
-      <li>Generate a target</li>
-      <li>Start the clock for a 30 second timer</li>
-      <li>Enter Your Answer into the input field</li>
-      <li>10 points awarded for correct answers only</li>
+      <li>6 Numbers Small (1-10) and Big (25,50,75,100)</li>
+      <li>Generate a target & Start the clock for a 30 second timer</li>
+      <li>Each number can only be used once</li>
+      <li>Only the (+ - * /) mathematical operations are allowed;</li>
+      <li>10 points for correct answers only</li>
     </ul>
   );
 };
